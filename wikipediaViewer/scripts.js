57e442b9f8d20c0258f46ce58f2317f81296761b
eigakow <iga.kowalska@gmail.com>
2017-05-05 13:31:29 +0200

    first release wikipediaViewer

---
+++ wikipediaViewer/scripts.js
@@ -0,0 +1,39 @@
+//var search="poland";
+// var url="https://en.wikipedia.org/w/api.php?action=query&titles=" + search + "&rvprop=content&format=json";
+
+$(document).ready(function() {
+  $( "form" ).submit(function( event ) {
+    // prevent the page reloading:
+    event.preventDefault();
+    // alert( "Handler for .submit() called." );
+    var search = $( "#searchPhrase" ).val();
+    console.log(search);
+
+    if (search !== null && search !== undefined && search !== ""){
+      //Remove old search
+      $( ".panel-info" ).remove();
+
+      //API details at https://www.mediawiki.org/wiki/API:Opensearch
+      var url="https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=" + search +
+      "&callback=?";
+
+      $.getJSON(url, (function(data) {
+        console.log(data);
+        for (i=0; i<data[1].length ; i++) {
+          var link = data[3][i];
+          var title = data[1][i];
+          var desc = data[2][i];
+          $("#searchResult").append( //[1]title [2]header [3]link
+            '<a href="'+ link +'" target="_blank"><div class="panel panel-default"><div class="panel-body">' +
+            '<h4 class="maintitle">' + title + '</h4><span class="subtitle">' + desc +
+            '</span></div></div></a>');
+          }
+      }
+    )).fail(function(d, textStatus, error) {
+      console.error("getJSON failed, status: " + textStatus + ", error: "+error)
+      });
+    }
+
+  });
+
+})
